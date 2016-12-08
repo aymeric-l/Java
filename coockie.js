e722fa7deb802920aeecfd6047aa4eee2589eebc
aymeric-l <aymeric-lacoste@hotmail.fr>
2016-12-08 11:30:52 +0100

    correction de bug

--- coockie.js
+++ coockie.js
@@ -1,27 +1,36 @@
-var prenom, resultat, form, question;
+var prenom, resultat, form, question, chaussette;
 prenom = prompt("Indiquez votre prénom");
 resultat = (" le roi des animaux");
 
-if(prenom == "julien") {
-	form = prompt("julien le formateur ? oui ou non ?");
+if(prenom == "robert"){
+	alert(prenom + resultat);
+
 }
-if(form == "oui") {
-	alert("c'est le meilleur !");
+
+else if(prenom == "julien") {
+	form = prompt("julien le formateur ? oui ou non ?");
+		if(form == "oui") {
+		alert("c'est le meilleur !");
+			}
+		else if(form == "non") {
+	alert("je le savais");
+			}
+}
+else if(prenom =="hasna"){
+	question = prompt("Oh, hasna ! Veux tu que je supprime tout tes fichiers ? oui ou non ?");
+		if(question== "oui") {
+	alert("Pas de soucis, j'arrive !");
+		}
+		else if(question == "non") {
+	alert("Dommage :(");
+		}
 }
 
 else if(prenom == "robert"){
 	alert(prenom + resultat);
 }
-if(prenom =="hasna"){
-	question = prompt("Oh, hasna ! Veux tu que je supprime tout tes fichiers ? oui ou non ?");
-}
-if (question == "oui"){
-	alert("Aucun problème ! Viens par ici je m'en occupe !");
 
-}
-else if(question == "non"){
-	alert("Okok dommage");
-}
+
 else
 {
 	alert("J'aime que les robert");
